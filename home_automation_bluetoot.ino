552ec2f2d4264fa45f26a482ae162ea70ece3a64
Rishabh Agrawal <43053152+arishabh@users.noreply.github.com>
2018-09-08 20:58:44 -0400

    Add files via upload

---
+++ home_automation_bluetoot.ino
@@ -0,0 +1,32 @@
+boolean flag = 0;
+int led = 2;
+int led1 = 3;
+char val = 0;
+int led2 = 13;
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  Serial.begin(38400);
+}
+
+void loop() { 
+  if(Serial.available()){
+    val = Serial.read();
+    Serial.print(val);
+  }
+  if(val == 'a'){
+    flag = !flag;
+    Serial.println(flag);
+    if (flag == 0){
+      digitalWrite(led, LOW);
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+    }
+    if (flag == 1){
+      digitalWrite(led, HIGH);
+      digitalWrite(led1, LOW);
+    }
+    delay(1000);
+    val = 0;
+  }
+}
